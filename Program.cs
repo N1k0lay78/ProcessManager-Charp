72a7a06e08f28d47cc2b7e248d394a68840a95d0
Rjkzavr <kolya.toropof@gmail.com>
2022-01-24 22:49:56 +0300

    feature make UI
    feature UI class
    feature ask_command() - to ask command
    feature update() - Handling user input
    feature run() - start console applicartion (work while running is True)
    commands:
    Quit                         - Close the application
    Kill <process>         - Kill process                             (name/id)
    TaskList                    - List of running process
    Process <process> - Show processes                      (name/id)
    Info <id>                - Show full info about process  (id)

--- Program.cs
+++ Program.cs
@@ -6,12 +6,8 @@ namespace ProcessManager
     {
         static void Main(string[] args)
         {
-            var pm = new ProcessManager();
-            pm.TrackList();
-            pm.GetProcessId(Console.ReadLine());
-            pm.KillById(Convert.ToInt32(Console.ReadLine()));
-            pm.KillByName(Console.ReadLine());
-            pm.GetProcessName(Convert.ToInt32(Console.ReadLine()));
+            var app = new UI();
+            app.run();
         }
     }
 }
---
+++ UI.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProcessManager
+{
+    public class UI
+    {
+        bool running = true;
+        public UI()
+        {
+            var pm = new ProcessManager();
+
+        }
+
+        string[] ask_comand()
+        {
+            Console.Write("command> ");
+            var ans = Console.ReadLine();
+            return ans.Split();
+        }
+
+        void update()
+        {
+            var command = ask_comand();
+            switch (command[0].ToLower())
+            {
+                case "quit":
+                    running = false;
+                    break;
+                case "help":
+                    Console.WriteLine("\tQuit              - Выйти из приложения");
+                    Console.WriteLine("\tKill <process>    - Убить процесс (name/id)");
+                    Console.WriteLine("\tTaskList          - Список выполняющихся задач");
+                    Console.WriteLine("\tProcess <process> - Показать процессы (name/id)");
+                    Console.WriteLine("\tInfo <id>         - Полную информацию о процессе");
+                    break;
+                case "kill":
+                    if (command.Length > 1)
+                    {
+                        Console.WriteLine($"*kill process {command[1]}*");
+                    }
+                    else
+                    {
+                        Console.WriteLine("missing required attribute 'process' (name/id)");
+                    }
+                    break;
+                case "tasklist":
+                    Console.WriteLine($"*taskList of processes*");
+                    break;
+                case "process":
+                    if (command.Length > 1)
+                    {
+                        Console.WriteLine($"*processes {command[1]}*");
+                    }
+                            else
+                    {
+                        Console.WriteLine("missing required attribute 'process' (name/id)");
+                    }
+                    break;
+                case "info":
+                    if (command.Length > 1)
+                    {
+                        Console.WriteLine($"*info about process id +{command[1]}*");
+                    }
+                    else
+                    {
+                        Console.WriteLine("missing required attribute 'id' (id)");
+                    }
+                    break;
+                default:
+                    Console.WriteLine("write help to get help");
+                    break;
+            }
+        }
+
+        public void run()
+        {
+            while (running)
+            {
+                update();
+            }
+        }
+    }
+}
